8ae78e20823e05de210643be307d95d73a1feaf6
syedmkazmi <syed.kazmi@kmandt.com>
2016-12-03 12:15:48 +0000

    initial commit

---
+++ app_server/controllers/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by syedkazmi on 03/12/2016.
+ */
+
+module.exports.index = function (req,res) {
+    res.render('index', { title: 'Address Book' });
+}
---
+++ app_server/models/db.js
@@ -0,0 +1,5 @@
+/**
+ * Created by syedkazmi on 03/12/2016.
+ */
+
+
